3d3f02735472146ab04c09fa98ea28ffdbe779f5
elormcent <innocent.fiadu@htu.edu.gh>
2019-07-25 16:55:22 +0000

    result almost perfect

--- myapp.py
+++ myapp.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 app = Flask(__name__)
 import pyowm
 from gpiozero import LED
@@ -10,18 +10,19 @@ def index():
     return render_template('home.html')
 
 
-@app.route("/weather", methods=['GET', 'POST'])
+@app.route("/weather")
 def weather():
     return render_template('weather.html')
 
 
 @app.route("/weather/submit", methods=['GET', 'POST'])
 def submit():
-    user_input_1=str(request.form.get('city'))
-    user_input_2=str(request.form.get('country'))
+    user_input_1=request.form.get('city')
+    user_input_2=request.form.get('country')
         
-    place = user_input_1 +', '+ user_input_2
-#    print(place)
+    place = user_input_1 + ", " + user_input_2
+    print(place)
+
     apikey = '51c6723dd2f51626dd33896729e79676'
     owm = pyowm.OWM(apikey)
     observation = owm.weather_at_place(place)
@@ -35,13 +36,14 @@ def submit():
     ran = w.get_rain()
     snow = w.get_snow()
     
-    tem = {"wind" : winds, "humidity" : humidities, "temperature" : tempreture, "pressure" : presh, "cloud" : clud, "rain" : ran, "snow" : snow}
+    tem = {"City": user_input_1, "Country": user_input_2, "Wind" : winds, "Humidity" : humidities, "Temperature" : tempreture, "Pressure" : presh, "Cloud" : clud, "Rain" : ran, "Snow" : snow}
     
     
-#    tem = winds, humidities, tempreture, presh, clud, ran, snow
-#    tem = []
     return render_template('submit.html', index = tem)
  
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
